a33a7538c29adb6025e021966cd3794d206009bc
”Shiwen <fairy3219889700@gmail.com>
2023-04-30 16:53:22 -0300

    update main.java

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        for (double i = 0.1; i < 0.2; i += 0.1) {
+        for (double i = 0.2; i < 0.3; i += 0.1) {
 
             String repoName = "eclipse.pde.ui";
             ArrayList<Integer> selectedBugs = SelectedBugs.loadSelectedBugs(repoName);
